272527e0f59e24b4a31deb9e9b29e124ee464b47
tadakazumoriba <tadakazumoriba@gmail.com>
2016-10-18 04:41:56 +0000

    followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,11 @@ class User < ActiveRecord::Base
                                     dependent:   :destroy
   has_many :following_users, through: :following_relationships, source: :followed
   
+  has_many :follower_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+  has_many :follower_users, through: :follower_relationships, source: :follower
+  
   #　他のユーザーをフォローする
   def follow(other_user)
     following_relationships.find_or_create_by(followed_id: other_user.id)
@@ -29,6 +34,7 @@ class User < ActiveRecord::Base
   def following?(other_user)
     following_users.include?(other_user)
   end
+
   
   def feed_items
     Micropost.where(user_id: following_user_ids + [self.id])
